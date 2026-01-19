b12d123ffa792bcbfe501187d11f81cb5058e76b
Loris Polenz <contact@lorispolenz.com>
2026-01-19 22:40:33 +0100

    fix(db): add helpers package

---
+++ helpers/env.go
@@ -0,0 +1,21 @@
+package helpers
+
+import (
+	"log"
+	"os"
+)
+
+func VerifyEnv() {
+	requiredVars := []string{
+		"S3_ENDPOINT",
+		"S3_ACCESS_KEY",
+		"S3_SECRET_KEY",
+		"GTFS_FP_BUCKET",
+	}
+
+	for _, v := range requiredVars {
+		if _, exists := os.LookupEnv(v); !exists {
+			log.Fatalf("Environment variable %s is not set\n", v)
+		}
+	}
+}
